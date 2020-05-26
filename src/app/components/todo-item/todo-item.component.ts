15660b1fe20398860ddf160db6fd616baea3c4c9
Alex <acooupe@gmail.com>
2020-05-26 14:04:05 +0100

    Making a put request to update todos

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,7 +10,8 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
 
   @Input() todo: Todo;
-  constructor() { }
+
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
   }
@@ -24,6 +26,7 @@ export class TodoItemComponent implements OnInit {
 
   onToggle(todo:Todo) : void {
     todo.isComplete = !todo.isComplete;
+    this.todoService.toggleComplete(todo).subscribe(todo => console.log(todo));
   }
 
   onDelete(todo: Todo) : void {
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,16 +3,29 @@ import { Todo } from '../models/Todo';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
+  httpOptions:object = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
   constructor(private http: HttpClient) { }
 
   todosURL:string = 'https://jsonplaceholder.typicode.com/todos';
   limit: string = '?_limit=5';
+
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosURL}${this.limit}`);
+  }
+
+  toggleComplete(todo: Todo): Observable<any> {
+    return this.http.put<Todo>(`${this.todosURL}/${todo.id}`,todo, this.httpOptions);
   }
 }
